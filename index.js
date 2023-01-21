719a18121cb2f9f6f61e6420c6cc9aa532fc95b8
njiddasalifu <njiddasalifu@gmail.com>
2023-01-21 15:04:59 +0100

    working with prompt to get imput from user and return a message with the entered value

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-let name; // this is a declaration of a variable which cant be a reserved keyword
-// a variable should also be meaningfull
-// a variable can not start with a number
-// cant not contain a space or hyphen (-)
-// are case-sensitive
 
-let fisrtname, lasttname;
+var name=prompt("Enter your firstname","");
+if(name!=null) {
+    document.getElementById("welcome").innerHTML =
+     "Hello " + name + "! How ae you doing today";
+
+}
