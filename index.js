f13a469751df5c42919e716deefcce8dd9caf0e4
njiddasalifu <njiddasalifu@gmail.com>
2023-01-21 14:28:26 +0100

    Working with the let keyword to delare variables

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let name; // this is a declaration of a variable which cant be a reserved keyword
+// a variable should also be meaningfull
+// a variable can not start with a number
+// cant not contain a space or hyphen (-)
+// are case-sensitive
+
+let fisrtname, lasttname;
