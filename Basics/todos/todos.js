c5c30d4c65f20f82937fed0e4840a9e0a4bf28d3
njiddasalifu <njiddasalifu@gmail.com>
2023-02-01 10:07:23 +0100

    map function learned

---
+++ Basics/todos/todos.js
@@ -0,0 +1,22 @@
+const todos= [{
+    id: 1,
+    text: ' Take AWS educate course',
+    isCompleted: false
+},
+{
+    id: 2,
+    text: ' Server setup',
+    isCompleted: false
+},
+{
+    id: 3,
+    text: ' Start reactjs',
+    isCompleted: false
+},
+];
+const todoText =todos.map(function(todo) {
+    return todo.text;
+});
+document.getElementById("todos").innerHTML=todoText;
+
+//console.log(todos);
