4e040095e0beeb59275e30fa9a8c533914d6f4c4
njiddasalifu <njiddasalifu@gmail.com>
2023-02-12 13:20:40 +0100

    prime number checker added

--- Basics/primeNumber.js
+++ Basics/primeNumber.js
@@ -2,7 +2,7 @@ function isPrime(n){
     if(n<2){
         return false
     }
-    for(let i=2;i<=Math.sqrt(ns);i++){
+    for(let i=2;i<=Math.sqrt(n);i++){
         if(n%i===0){
             return false
         }
