a86a9bc8e9b2aa7ee8e17abd4d6905c3ed580ae0
njiddasalifu <njiddasalifu@gmail.com>
2023-02-12 12:13:07 +0100

    sum of n numbers addedin the basics dir

---
+++ nodejs/fileSystem/fs.js
@@ -0,0 +1,9 @@
+//const fs = require('fs')
+//fs.writeFileSync('note.txt', 'I live in Buea-Cameroon')
+
+
+// exporting files
+const check = function() {
+    console.log('Doing some work...')
+}
+module.exports = check
