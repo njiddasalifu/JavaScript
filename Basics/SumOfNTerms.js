1c3e6fe9233441bc592039f6c9637a79d78df467
Njidda Salifu <94807133+njiddasalifu@users.noreply.github.com>
2023-03-04 12:50:10 +0100

    Update SumOfNTerms.js

--- Basics/SumOfNTerms.js
+++ Basics/SumOfNTerms.js
@@ -1,8 +1,8 @@
-function summation(n) {
+function summation(n) { // summing function with a paramenter of n
     let sum = 0;
     for(let i=1;i<=n;i++){
         sum+=i;
     }
     return sum;
 }
-console.log.summation(4);
+console.log.summation(4); // results will be displayed on the console
