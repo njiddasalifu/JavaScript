8879355bd7604c91bb858f1703e35841a3cc9442
njiddasalifu <njiddasalifu@gmail.com>
2023-03-23 06:37:31 +0100

    working with time and date

---
+++ time/time.js
@@ -0,0 +1,18 @@
+
+const time = document.getElementById("time").innerHTML=
+function time(){
+    const date = new Date()
+    const hours = new date.getHours()
+    let timeOfDay 
+    if(hours<12) {
+        timeOfDay = "morning";
+    } else if(hours>=12 && hours<17){
+        timeOfDay = "afterneeon";
+    } else{
+        timeOfDay = "night";
+    }
+
+    return (
+        <h2>Good morning!</h2>
+    );
+}
