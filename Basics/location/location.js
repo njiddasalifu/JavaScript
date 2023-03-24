ee7e465ec7b1f2db15ea26f6881f38fb213f44e7
njiddasalifu <njiddasalifu@gmail.com>
2023-03-24 06:25:54 +0100

    showin results on a map

--- Basics/location/location.js
+++ Basics/location/location.js
@@ -31,4 +31,14 @@ function showError(error){
           break;
       }
 
+      // displaying results in a map
+      // To display the result in a map, you need access to a map service, like Google Maps.
+     // function showPosition(position){
+        let latlon = position.coords.latitude + "," + position.coords.longitude;
+
+       // let img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+       // "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_KEY";
+       // document.getElementById("mapholder").innerHTML = "<img src = '"+img_url +"'>";
+     // }
+
 }
