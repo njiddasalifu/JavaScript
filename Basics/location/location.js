5136f0e3883642d0ddc4f9657f2525907034db0d
njiddasalifu <njiddasalifu@gmail.com>
2023-03-24 06:18:43 +0100

    added a function to handle errors if any

--- Basics/location/location.js
+++ Basics/location/location.js
@@ -1,6 +1,9 @@
 
 const x = document.getElementById("demo");
-function getLocation(){
+function getLocation(){ 
+    // checking if Geoloacation si supported by the browser
+    // If supported, run the getCurrentPosition() method. If not, display a message to the user
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition);
     }else {
@@ -9,4 +12,23 @@ function getLocation(){
 }
 function showPosition(position) {
     x.innerHTML ="Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
+
+// handling errors
+function showError(error){
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+          x.innerHTML = "User denied the request for Geolocation."
+          break;
+        case error.POSITION_UNAVAILABLE:
+          x.innerHTML = "Location information is unavailable."
+          break;
+        case error.TIMEOUT:
+          x.innerHTML = "The request to get user location timed out."
+          break;
+        case error.UNKNOWN_ERROR:
+          x.innerHTML = "An unknown error occurred."
+          break;
+      }
+
 }
