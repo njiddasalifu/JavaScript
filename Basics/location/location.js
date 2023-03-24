67a96e625c8e0594c3a7a178d25e5a0805b7e59f
njiddasalifu <njiddasalifu@gmail.com>
2023-03-24 06:10:11 +0100

    working with location API to the location coordinates

---
+++ Basics/location/location.js
@@ -0,0 +1,12 @@
+
+const x = document.getElementById("demo");
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }else {
+        x.innerHTML = "Geolocations is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    x.innerHTML ="Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+}
