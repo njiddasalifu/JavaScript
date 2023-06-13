8f51356abe48dca17c4208fb339d5ff6179e62b7
njiddasalifu <njiddasalifu@gmail.com>
2023-06-13 21:18:33 +0100

    equality sign

--- Basics/conditionals.js
+++ Basics/conditionals.js
@@ -1,6 +1,6 @@
 let icecream = 'chocolate';
 
-if(icecream == chocolate){
+if(icecream === 'chocolate'){
     alert("Yah, I love chocolate ice cream!")
 } else{
     alert("Awww, but cchocolate is my favorite")
